47d9048958ab4f8dddad718cf7513891e69e21ed
usolha <usolha@kkg-do.eu>
2022-09-13 11:22:02 +0200

    fhgfhfhdfh

--- src/MainProgramm.java
+++ src/MainProgramm.java
@@ -1,7 +1,7 @@
 public class MainProgramm {
     public static void main (String[] args) {
         Employee e1 = new Employee("Stradi", "Teller", "20.04.2006"; )
-        Employee e2 = new Employee("Olha", "Mais", "31.12.2004"; );
+        Employee e2 = new Employee("Olha", "Mais", "31.12.2004"; )
 
         System.out.println(e1);
         System.out.println(e2);
